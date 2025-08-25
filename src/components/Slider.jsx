c83402e9313ae79f495b1a3829c0fea1100cc17d
NatalieChurch <nataliechurch12@gmail.com>
2025-08-24 21:00:13 -0500

    Added functionality to Slider component, added some generic content to the slides

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,10 +3,54 @@ import { useNavigate } from 'react-router-dom';
 import '../App.css';
 
 const slides = [
-    {},
-    {},
-    {},
-    {}
+    {
+
+        id: 1,
+        lineOne: "a",
+        lineTwo: "b",
+        lineThree: "c",
+        content: ["d"],
+        button: 'e',
+        link: '/f',
+        background: '/g.jpg',
+
+    },
+    {
+
+        id: 2,
+        lineOne: "a",
+        lineTwo: "b",
+        lineThree: "c",
+        content: ["d"],
+        button: 'e',
+        link: '/f',
+        background: '/g.jpg',
+
+    },
+    {
+
+        id: 3,
+        lineOne: "a",
+        lineTwo: "b",
+        lineThree: "c",
+        content: ["d"],
+        button: 'e',
+        link: '/f',
+        background: '/g.jpg',
+
+    },
+    {
+
+        id: 4,
+        lineOne: "a",
+        lineTwo: "b",
+        lineThree: "c",
+        content: ["d"],
+        button: 'e',
+        link: '/f',
+        background: '/g.jpg',
+
+    }
 ];
 
 
@@ -31,17 +75,78 @@ export default function Slider() {
 
 
     return (
-        <section></section>
+        <section id="sliderSection">
+            
+            <div id="sliderDiv">
 
+            {visibleSlides.map((slide, index) => (
+                <div key={slide.id}
+                className={`banner ${index === currentSlide ? 'active' : ''}`}
+                id={`top-banner-${slide.id}`}
+                >
 
+                    <div id="sliderInside">
+                        <h2>{slide.title}</h2>
 
+                        {slide.subtitle && (
+                        <h1>
+                            {slide.subtitle.split('\n').map((line, i) => (
+                                <span key={i}>
+                                    {line}
+                                    <br/>
+                                </span>
+                            ))}
+                        </h1>
+                        )}
 
+                        {slide.content && (
+                            Array.isArray(slide.content) ? (
+                            slide.content.map((p, i) => <p key={i}>{p}</p>)) 
+                            : (
+                                <p>{slide.content}</p>
+                            )
+                        )}
 
+                        {slide.list && (
+                          <ul>
+                            {slide.list.map((item, i) => (
+                                <li key={i}>{item}</li>
+                            ))}
+                          </ul>  
+                        )}
+                    </div>
+                </div>
+                ))}
 
+                <nav>
+                    <div className="bannerControls">
 
+                        {visibleSlides.map((slide, index) => (
+                            <label key={slide.id} 
+                            onClick={() => setCurrentSlide(index)}>
 
+                                <span className="progressBar">
+                                    <span
+                                        className="progressBarFill"
+                                        style = {{
+                                            animation: index === currentSlide ?
+                                            'progressBarFill 10s linear forwards'
+                                            : 'none',
+                                        }}>
+                                    </span>
+                                </span>
 
+                                <span>{`0${index+1}`}</span>
 
+                                {slide.lineOne}
 
+                            </label>
+                        ))}
+
+                    </div>
+                </nav>
+
+            </div>
+        </section>
     );
 }
