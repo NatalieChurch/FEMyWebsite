1541f90d81f43461148f0010b689d734079bcd9d
NatalieChurch <nataliechurch12@gmail.com>
2025-08-25 15:49:39 -0500

    Added minimal Home component function

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import Slider from "./Slider";
+
+export default function Home() {
+
+    return (
+
+        <>
+            <Slider/>
+
+            <main id = "homeMainContainer">
+                
+            </main>
+        </>
+    
+    )
+
+}
